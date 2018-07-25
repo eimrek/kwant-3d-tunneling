cf3b4d1ee6e70ec1a5e4bdbf750fdc6574662f77
Kristjan <kristjaneimre@gmail.com>
2018-07-25 03:47:20 +0300

    Add example for the cu adatom

---
+++ example/run_transmission_calc.sh
@@ -0,0 +1,9 @@
+
+../transmission_script.py \
+  --barrier_file ./vasp_cu_adatom_e0.4.npz \
+  --lat_param 0.05 \
+  --e_min 0.5 \
+  --e_max 8.0 \
+  --e_num 16 \
+  --e_leads 0.0 \
+  --out_dir ./output/
